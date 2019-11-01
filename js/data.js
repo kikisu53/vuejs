b3ed581ff74756b8ec78c6817641dd0fdc773384
kikisu53 <hcqp53@hotmail.com>
2019-11-01 20:40:30 +0800

    websotrage

---
+++ js/data.js
@@ -0,0 +1,5 @@
+var lists = [
+    { 'id': '123', 'name': 'product 1', 'urlPath': 'p1.html' },
+    { 'id': '456',  'name': 'product 2', 'urlPath': 'p2.html' },
+    { 'id': '789',  'name': 'product 3', 'urlPath': 'p3.html' },
+];
