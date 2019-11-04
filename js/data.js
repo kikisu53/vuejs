21ff7e54c57c9bf92d1308de91703bee0945af94
kikisu53 <hcqp53@hotmail.com>
2019-11-04 18:49:06 +0800

    second

--- js/data.js
+++ js/data.js
@@ -3,3 +3,7 @@ var lists = [
     { 'id': '456',  'name': 'product 2', 'urlPath': 'p2.html' },
     { 'id': '789',  'name': 'product 3', 'urlPath': 'p3.html' },
 ];
+
+//靜態網址 連字號 seo better
+// /sss/abc.html 靜態網址
+// /sss/p.html?p=abc 動態網址
